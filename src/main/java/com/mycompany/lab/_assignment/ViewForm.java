448adbe9bac5bdc5854bfbf6c772277fbee41603
mudrap2706 <pandya.mu@northeastern.edu>
2024-11-10 18:39:13 -0500

    Added JTable Component for Data Viewing

--- src/main/java/com/mycompany/lab/_assignment/ViewForm.java
+++ src/main/java/com/mycompany/lab/_assignment/ViewForm.java
@@ -1,4 +1,7 @@
 /*viewform 
+//Creates View page 
+//JTable Component for Data Viewing
+//Data Update and Delete Functionality
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
